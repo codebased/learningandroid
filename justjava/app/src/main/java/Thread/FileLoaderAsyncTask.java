a7058d46ef98d0bb03538bb1ec8d776943079fde
Amit Malhotra <codebased@hotmail.com>
2016-07-21 16:47:44 +1000

    Implementing Thread based file reading

---
+++ justjava/app/src/main/java/Thread/FileLoaderAsyncTask.java
@@ -0,0 +1,18 @@
+package thread;
+
+/**
+ * Created by codebased on 21/07/16.
+ */
+public class FileLoaderAsyncTask {
+
+    public static void main(String args[]) throws InterruptedException {
+
+        System.out.printf("MAIN METHOD WITH THREAD ID...%s\n", Thread.currentThread().getId());
+
+        FileLoaderRunnable fileLoaderAsyncTask = new FileLoaderRunnable();
+        fileLoaderAsyncTask.play();
+
+        System.out.println("MAIN METHOD END");
+    }
+
+}

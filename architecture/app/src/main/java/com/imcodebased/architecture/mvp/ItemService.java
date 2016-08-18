152bf04bab57d2c6f686e9ff5da6c4f48381c779
Amit Malhotra <codebased@hotmail.com>
2016-08-18 21:57:51 +1000

    Introducing eventbus

--- architecture/app/src/main/java/com/imcodebased/architecture/mvp/ItemService.java
+++ architecture/app/src/main/java/com/imcodebased/architecture/mvp/ItemService.java
@@ -1,12 +1,6 @@
 package com.imcodebased.architecture.mvp;
 
-import java.util.List;
-
 public interface ItemService {
 
-    interface ItemListener {
-        void onLoad(List<String> items);
-    }
-
-    void findItems(ItemListener listener);
+    void findItems();
 }

5b228d4ea2f8274ef0528607010544309b793fb4
Amit Malhotra <codebased@hotmail.com>
2016-09-10 15:23:56 +1000

    empty projects

---
+++ material/app/src/main/java/com/imcodebased/material/fragments/DetailsFragment.java
@@ -0,0 +1,19 @@
+package com.imcodebased.material.fragments;
+
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+
+/**
+ * Created by codebased on 7/09/16.
+ */
+public class DetailsFragment extends Fragment {
+
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_BACK && getSupportFragmentManager.
+//                getBackStackEntryCount > 1) {
+//            getSupportFragment.popBackStack();
+//            return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+}
---
+++ patterns/app/src/main/java/creation/factory.java
@@ -0,0 +1,7 @@
+package creation;
+
+/**
+ * Created by codebased on 10/09/16.
+ */
+public class factory {
+}
